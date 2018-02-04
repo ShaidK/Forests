ede0f25401f7c5042ed29fac70293e50fcffeca7
Shaid Khan <30567635+ShaidK@users.noreply.github.com>
2018-02-04 11:18:20 +0000

    V0.1.3
    Configure Database for Read & Write Operations

---
+++ app/src/main/java/com/shaidk/forests/database/data/NoteDao.java
@@ -0,0 +1,42 @@
+/*
+ * File: NoteDao.java | Note: Maintains the class to represent the Database Interactions
+ */
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Shaid Khan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.shaidk.forests.database.data;
+import com.shaidk.forests.database.entities.Note;
+import android.arch.persistence.room.*;
+import java.util.List;
+
+@Dao
+public interface NoteDao 
+{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertNewNote(Note... arg);
+
+    @Query(value = "SELECT * FROM NOTE")
+    List<Note> obtainAllNotes();
+}
